f77c99b39be3c92e01c68a2a40779012624aa927
edwin-wq <winisgomez@gmail.com>
2023-04-16 07:06:45 -0600

    validación de los campos para altas de libros

--- src/main/java/com/hardbug/bibliotecatenango/AltaLibrosController.java
+++ src/main/java/com/hardbug/bibliotecatenango/AltaLibrosController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import javax.swing.*;
 import java.util.Date;
 
 /* Clase AltaLibros:
@@ -24,24 +25,87 @@ public class AltaLibrosController {
     @FXML
     private Button Boton_Modo, BotonGuardar;
     @FXML
-    private TextField Campo_clasificacion, Campo_anio_edicion, Campo_registro_clasificacion, Campo_estante, Campo_existencias
+    private TextField  Campo_clasificacion, Campo_anio_edicion, Campo_registro_clasificacion, Campo_estante, Campo_existencias
             , Campo_editorial, Campo_lugar_edicion, Campo_titulo_libro, Campo_nombre_autor, Campo_descripcion_libro;
     public AnchorPane Fondo;
 
 
-
-    //El método causará errores al correrse actualmente, se debe validar
+                                //El método causará errores al correrse actualmente, se debe validar
     @FXML
     void GuardarLibro(ActionEvent event){
         /* Aquí se obtendrá la información de los campos de texto y se validarán, posteriormente se mandará
             a almacenar en la base de datos
          */
-        if (camposValidos()){
-            System.out.println("Campos correctos y bien llenados");
-        }else{
-            System.out.println("Campos con errores o no llenados");
+        if (Campo_clasificacion.getText().matches("\\b[A-Z][a-z]*\\b")) {
+
+            if ( Campo_registro_clasificacion.getText().matches("^\\d{3}") || Campo_registro_clasificacion.getText().matches("^S\\/C")) {
+
+                if ( Campo_estante.getText().matches("^[A-Z]{1,6}-\\d+")) {
+
+                    if (Campo_existencias.getText().matches("^\\d{1,8}")) {
+
+                        if ( Campo_editorial.getText().matches("^[A-Za-z\\s&-]+")) {
+
+                            if ( Campo_lugar_edicion.getText().matches("^[A-Za-z]+[\\s,.'-]*")) {
+
+                                if ( Campo_nombre_autor.getText().matches("^[A-Z][a-z]+(\\s[A-Z][a-z]+){1,3}")) {
+
+                                    if ( Campo_titulo_libro.getText().matches("^[A-Z][a-z]+(\\s[a-z]+){1,15}")) {
+
+                                        if ( Campo_anio_edicion.getText().matches("^\\d{4}")) {
+
+                                            if (!Campo_descripcion_libro.getText().isEmpty()) {
+
+                                                JOptionPane.showMessageDialog(null, "Campos correctos y bien llenados");
+
+                                            } else {
+                                               // System.out.println("Corrija los datos ingresados en el campo Descripción del libro ");
+                                                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Descripción del libro\n Ejemplo: En este libro podemos encontra los temas...");
+                                            }
+                                        } else {
+                                            //System.out.println("Corrija los datos ingresados en el campo Registro de Año de edición");
+                                            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Registro de Año de edición\n Ejemplo: 1943");
+                                        }
+                                    } else {
+                                        //System.out.println("corrija los datos ingresados en el campo Titulo del libro");
+                                        JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Titulo del libro\n Ejemplo: El principito");
+                                    }
+                                } else {
+                                    //System.out.println("corrija los datos ingresados en el campo Nombre del autor");
+                                    JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Nombre del autor\n Ejemplo: Antoine De Saint Exupéry");
+                                }
+                            } else {
+                                //System.out.println("corrija los datos ingresados en el campo Lugar de edición");
+                                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Lugar de edición \n Ejemplo: Francia");
+                            }
+                        } else {
+                            //System.out.println("corrija los datos ingresados en el campo Editorial");
+                            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Editorial \n Ejemplo: Reynal & Hitchcock");
+                        }
+                    } else {
+                       //System.out.println("corrija los datos ingresados en el campo Existencias");
+                        JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Existencias\n Ejemplo: 5");
+                    }
+                } else {
+                    //System.out.println("corrija los datos ingresados en el campo Estante");
+                    JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Estante\n Ejemplo: AAAAAA-000000 ");
+                }
+            } else {
+               //System.out.println("corrija los datos ingresados en el campo Registro de clasificación");
+                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Registro de clasificación\n Ejemplo: 000 al 900 o S/C");
+            }
+        } else {
+           //System.out.println("corrija los datos ingresados en el campo Clasificación");
+            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Clasificación\n Ejemplo: Historia, Ciencias, Álgebra, etc.");
         }
     }
+
+
+
+
+
+
+
 
     boolean camposValidos(){
         String clasificacion = Campo_clasificacion.getText();
@@ -60,6 +124,10 @@ public class AltaLibrosController {
             return false;
         }
     }
+
+
+
+
 
 
 
