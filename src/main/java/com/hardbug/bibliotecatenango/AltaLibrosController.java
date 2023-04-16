27b795a0ec5ea0a87eb983990b66ebbe689a41e9
edwin-wq <winisgomez@gmail.com>
2023-04-16 07:33:29 -0600

    agregue comentarios

--- src/main/java/com/hardbug/bibliotecatenango/AltaLibrosController.java
+++ src/main/java/com/hardbug/bibliotecatenango/AltaLibrosController.java
@@ -29,74 +29,62 @@ public class AltaLibrosController {
             , Campo_editorial, Campo_lugar_edicion, Campo_titulo_libro, Campo_nombre_autor, Campo_descripcion_libro;
     public AnchorPane Fondo;
 
-
-                                //El método causará errores al correrse actualmente, se debe validar
     @FXML
     void GuardarLibro(ActionEvent event){
-        /* Aquí se obtendrá la información de los campos de texto y se validarán, posteriormente se mandará
-            a almacenar en la base de datos
+        /* Aquí se obtendrá la información de los campos de texto y se validarán,
+        posteriormente se mandará a almacenar en la base de datos.
          */
-        if (Campo_clasificacion.getText().matches("\\b[A-Z][a-z]*\\b")) {
-
+        //Valida una palabra iniciando por mayúscula
+        if ( Campo_clasificacion.getText().matches("\\b[A-Z][a-z]*\\b")) {
+            //Valida tres números del 000 al 999 o S/C
             if ( Campo_registro_clasificacion.getText().matches("^\\d{3}") || Campo_registro_clasificacion.getText().matches("^S\\/C")) {
-
+                //Valida de 1 a 6 letras A-Z seguida de un gion medio seguido de números n
                 if ( Campo_estante.getText().matches("^[A-Z]{1,6}-\\d+")) {
-
-                    if (Campo_existencias.getText().matches("^\\d{1,8}")) {
-
+                    //Valida números hasta una cantidad de 1 a 8
+                    if ( Campo_existencias.getText().matches("^\\d{1,8}")) {
+                        //Valida una o mas palaras con espacios y simbolos & -
                         if ( Campo_editorial.getText().matches("^[A-Za-z\\s&-]+")) {
-
+                            //Valida una o mas palaras con espacios y simbolos , . ´- -
                             if ( Campo_lugar_edicion.getText().matches("^[A-Za-z]+[\\s,.'-]*")) {
-
-                                if ( Campo_nombre_autor.getText().matches("^[A-Z][a-z]+(\\s[A-Z][a-z]+){1,3}")) {
-
+                                //Valida de 1 a 4 palabras separadas por espacios iniciadas por mayúsculas
+                                if ( Campo_nombre_autor.getText().matches("^[A-Z][a-z]+(\\s[A-Z][a-z]+){1,4}")) {
+                                    //Valida de 1 a 15 palabras separadas por espacios iniciada solo la primera por mayúscula
                                     if ( Campo_titulo_libro.getText().matches("^[A-Z][a-z]+(\\s[a-z]+){1,15}")) {
-
+                                        //Valida 4 números
                                         if ( Campo_anio_edicion.getText().matches("^\\d{4}")) {
-
+                                            //Valida que no este vacío el campo
                                             if (!Campo_descripcion_libro.getText().isEmpty()) {
 
-                                                JOptionPane.showMessageDialog(null, "Campos correctos y bien llenados");
-
+                                                JOptionPane.showMessageDialog(null, "Campos correctos y bien llenados.");
                                             } else {
-                                               // System.out.println("Corrija los datos ingresados en el campo Descripción del libro ");
-                                                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Descripción del libro\n Ejemplo: En este libro podemos encontra los temas...");
+                                                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Descripción del libro.\n Ejemplo: En este libro podemos encontra los temas...");
                                             }
                                         } else {
-                                            //System.out.println("Corrija los datos ingresados en el campo Registro de Año de edición");
-                                            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Registro de Año de edición\n Ejemplo: 1943");
+                                            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Registro de Año de edición.\n Ejemplo: 1943");
                                         }
                                     } else {
-                                        //System.out.println("corrija los datos ingresados en el campo Titulo del libro");
-                                        JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Titulo del libro\n Ejemplo: El principito");
+                                        JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Titulo del libro.\n Ejemplo: El principito");
                                     }
                                 } else {
-                                    //System.out.println("corrija los datos ingresados en el campo Nombre del autor");
-                                    JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Nombre del autor\n Ejemplo: Antoine De Saint Exupéry");
+                                    JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Nombre del autor.\n Ejemplo: Antoine De Saint Exupéry");
                                 }
                             } else {
-                                //System.out.println("corrija los datos ingresados en el campo Lugar de edición");
-                                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Lugar de edición \n Ejemplo: Francia");
+                                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Lugar de edición. \n Ejemplo: Francia");
                             }
                         } else {
-                            //System.out.println("corrija los datos ingresados en el campo Editorial");
-                            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Editorial \n Ejemplo: Reynal & Hitchcock");
+                            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Editorial. \n Ejemplo: Reynal & Hitchcock");
                         }
                     } else {
-                       //System.out.println("corrija los datos ingresados en el campo Existencias");
-                        JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Existencias\n Ejemplo: 5");
+                        JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Existencias.\n Ejemplo: 5");
                     }
                 } else {
-                    //System.out.println("corrija los datos ingresados en el campo Estante");
-                    JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Estante\n Ejemplo: AAAAAA-000000 ");
+                    JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Estante.\n Ejemplo: AAAAAA-000000 ");
                 }
             } else {
-               //System.out.println("corrija los datos ingresados en el campo Registro de clasificación");
-                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Registro de clasificación\n Ejemplo: 000 al 900 o S/C");
+                JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Registro de clasificación.\n Ejemplo: 000 al 900 o S/C");
             }
         } else {
-           //System.out.println("corrija los datos ingresados en el campo Clasificación");
-            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Clasificación\n Ejemplo: Historia, Ciencias, Álgebra, etc.");
+            JOptionPane.showMessageDialog(null, "Corrija los datos ingresados en el campo Clasificación.\n Ejemplo: Historia, Ciencias, Álgebra, etc.");
         }
     }
 
