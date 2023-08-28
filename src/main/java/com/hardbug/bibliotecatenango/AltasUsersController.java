d7cc4111e17c2bb715899173d2f3953036e1054d
CarlitosHV <carloshvsa21@hotmail.com>
2023-08-27 20:10:53 -0600

    Fusión de lo del Car y el Wilis

--- src/main/java/com/hardbug/bibliotecatenango/AltasUsersController.java
+++ src/main/java/com/hardbug/bibliotecatenango/AltasUsersController.java
@@ -82,6 +82,8 @@ public class AltasUsersController extends BDController implements Initializable 
     Estados estados = new Estados();
     Municipios municipios = new Municipios();
     BDController bd = new BDController();
+
+    Alertas alertas = new Alertas();
     Usuario mUsuario;
     public static Usuario editUsuario;
     private Stage modalStage;
@@ -91,41 +93,200 @@ public class AltasUsersController extends BDController implements Initializable 
     }
 
     public static int OPERACION = 1;
+    /* Variables booleanas que marcan si los campos están correctos */
+    private static boolean Titulo_correo_bol, Contrasenia_bol, Curp_bol, Telefono_bol, Nombre_bol, Edad_bol,
+            Apellido_paterno_bol, Apellido_materno_bol, Calle_bol, Codigo_bol;
 
-    public void validar_correo(KeyEvent keyEvent) {
+    boolean camposValidos() {
+        if (Titulo_correo_bol && Contrasenia_bol && Curp_bol && Telefono_bol && Nombre_bol && Edad_bol &&
+                Apellido_paterno_bol && Apellido_materno_bol && Calle_bol && Codigo_bol
+        ) {
+
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    public void validar_correo() {
+        if (Campo_correo.isEditable()) {
+            if (Campo_correo.getText().matches("\\b[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}\\b")
+                    && !Campo_correo.getText().isEmpty()) {
+                Titulo_correo_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_correo.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_correo.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_correo.setStyle("-fx-border-color: red");
+                Titulo_correo_bol = false;
+            }
+        }
     }
 
-    public void validar_contrasenia(KeyEvent keyEvent) {
+    //valida campos entre 6 y 12 carecteres y deve contener un número almenos con minuscusculas io mayúsculas o carecteres especiales
+    public void validar_contrasenia() {
+        if (Campo_contrasenia.isEditable()) {
+            if (Campo_contrasenia.getText().matches("^(?=.*\\d)(?=.*[a-zA-Z]).{6,12}$")
+                    && !Campo_contrasenia.getText().isEmpty()) {
+                Contrasenia_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_contrasenia.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_contrasenia.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_contrasenia.setStyle("-fx-border-color: red");
+                Contrasenia_bol = false;
+            }
+        }
     }
 
-    public void validar_curp(KeyEvent keyEvent) {
+    public void validar_curp() {
+        if (Campo_curp.isEditable()) {
+            if (Campo_curp.getText().matches("^[A-Z]{4}\\d{6}[HM]{1}[A-Z]{5}[A0-9]{2}$")
+                    && !Campo_curp.getText().isEmpty()) {
+                Curp_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_curp.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_curp.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_curp.setStyle("-fx-border-color: red");
+                Curp_bol = false;
+            }
+        }
     }
 
-    public void validar_telefono(KeyEvent keyEvent) {
+    public void validar_telefono() {
+        if (Campo_telefono.isEditable()) {
+            if (Campo_telefono.getText().matches("^\\d{10}$")
+                    && !Campo_telefono.getText().isEmpty()) {
+                Telefono_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_telefono.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_telefono.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_telefono.setStyle("-fx-border-color: red");
+                Telefono_bol = false;
+
+            }
+        }
     }
 
-    public void validar_nombre(KeyEvent keyEvent) {
+    public void validar_nombre() {
+        if (Campo_nombre.isEditable()) {
+            if (Campo_nombre.getText().matches("^[A-Z][a-záéíóúñ]+(\\s[A-Z][a-záéíóúñ]+){0,4}$")
+                    && !Campo_nombre.getText().isEmpty()) {
+                Nombre_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_nombre.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_nombre.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_nombre.setStyle("-fx-border-color: red");
+                Nombre_bol = false;
+
+
+            }
+        }
     }
 
-    public void validar_edad(KeyEvent keyEvent) {
+    public void validar_edad() {
+        if (Campo_edad.isEditable()) {
+            if (Campo_edad.getText().matches("^(?:[1-9][0-9]?|100)$")
+                    && !Campo_edad.getText().isEmpty()) {
+                Edad_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_edad.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_edad.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_edad.setStyle("-fx-border-color: red");
+                Edad_bol = false;
+
+
+            }
+        }
     }
 
-    public void validar_apellido_paterno(KeyEvent keyEvent) {
+    public void validar_apellido_paterno() {
+        if (Campo_apellido_paterno.isEditable()) {
+            if (Campo_apellido_paterno.getText().matches("^[A-Z][a-záéíóúñ]+(\\s[A-Z][a-záéíóúñ]+){0,4}$")
+                    && !Campo_apellido_paterno.getText().isEmpty()) {
+                Apellido_paterno_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_apellido_paterno.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_apellido_paterno.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_apellido_paterno.setStyle("-fx-border-color: red");
+                Apellido_paterno_bol = false;
+
+
+            }
+        }
     }
 
-    public void validar_apellido_materno(KeyEvent keyEvent) {
+
+    public void validar_apellido_materno() {
+        if (Campo_apellido_materno.isEditable()) {
+            if (Campo_apellido_materno.getText().matches("^[A-Z][a-záéíóúñ]+(\\s[A-Z][a-záéíóúñ]+){0,4}$")
+                    && !Campo_apellido_materno.getText().isEmpty()) {
+                Apellido_materno_bol = true;
+                if (IndexApp.TEMA == 1) {
+                    Campo_apellido_materno.setStyle("-fx-border-color: #595b5d");
+                } else {
+                    Campo_apellido_materno.setStyle("-fx-border-color: black");
+                }
+            } else {
+                Campo_apellido_materno.setStyle("-fx-border-color: red");
+                Apellido_materno_bol = false;
+
+
+            }
+        }
     }
 
-    public void validar_ocupacion(KeyEvent keyEvent) {
+
+    public void validar_calle() {
+        if (Campo_calle.isEditable()) {
+            if (Campo_calle.getText().matches("^[A-Z][a-záéíóúñ]+(\\s[A-Z][a-záéíóúñ]+){1,4}$")
+                    && !Campo_calle.getText().isEmpty())
+            {
+                Calle_bol = true; if (IndexApp.TEMA == 1) {
+                Campo_calle.setStyle("-fx-border-color: #595b5d");
+            } else {
+                Campo_calle.setStyle("-fx-border-color: black");
+            }   } else{
+                Campo_calle.setStyle("-fx-border-color: red");
+                Calle_bol = false;
+
+            }
+        }
     }
 
-    public void validar_codigo(KeyEvent keyEvent) {
-    }
-
-    public void validar_calle(KeyEvent keyEvent) {
-    }
-
-    public void AccionesBotonGuardar(ActionEvent actionEvent) {
+    public void validar_codigo() {
+        if (Campo_codigo.isEditable()) {
+            if (Campo_codigo.getText().matches("^\\d{4,5}$")
+                    && !Campo_codigo.getText().isEmpty())
+            {
+                Codigo_bol = true; if (IndexApp.TEMA == 1) {
+                Campo_codigo.setStyle("-fx-border-color: #595b5d");
+            } else {
+                Campo_codigo.setStyle("-fx-border-color: black");
+            }   } else {
+                Campo_codigo.setStyle("-fx-border-color: red");
+                Codigo_bol = false;
+            }
+        }
     }
 
     @Override
@@ -209,6 +370,7 @@ public class AltasUsersController extends BDController implements Initializable 
         });
 
 
+
         //crea un hipervinculo para buscar en curp
         Hyperlink_curp.setOnAction(actionEvent -> {
             Desktop desktop = Desktop.getDesktop();
@@ -242,55 +404,141 @@ public class AltasUsersController extends BDController implements Initializable 
         });
 
         BotonGuardar.setOnAction(event -> {
-            int IdNombre = 0;
-            int IdDir = 0;
-            mUsuario = new Usuario();
-            if (editUsuario != null) {
-                mUsuario.IdUsuario = editUsuario.IdUsuario;
-                IdNombre = editUsuario.nombre.getIdNombre();
-                IdDir = editUsuario.direccion.IdDireccion;
-            }
-            mUsuario.setNombre(Campo_nombre.getText().trim());
-            mUsuario.setApellidoPaterno(Campo_apellido_paterno.getText().trim());
-            mUsuario.setApellidoMaterno(Campo_apellido_materno.getText().trim());
-            mUsuario.setSexo(Combo_sexo.getValue());
-            mUsuario.setEdad(Integer.parseInt(Campo_edad.getText().trim()));
-            mUsuario.setCorreo(Campo_correo.getText().trim());
-            Nombres nombre = new Nombres(IdNombre, mUsuario.getNombre(), mUsuario.getApellidoPaterno(), mUsuario.getApellidoMaterno());
-            mUsuario.nombre = nombre;
-            try {
-                String contraseniacifrada = ClaseCifrarContrasenia.encript(Campo_contrasenia.getText().trim());
-                mUsuario.setContrasenia(contraseniacifrada);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-            mUsuario.setCurp(Campo_curp.getText().trim());
-            mUsuario.setTelefono(new BigInteger(Campo_telefono.getText().trim()));
-            mUsuario.setOcupacion(Combo_ocupacion.getValue());
-            mUsuario.setGradoEscolar(Combo_grado.getValue());
-            mUsuario.setCodigoPostal(Integer.parseInt(Campo_codigo.getText().trim()));
-            mUsuario.setEstado(Combo_estado.getValue());
-            mUsuario.setMunicipio(Combo_municipio.getValue());
-            mUsuario.setLocalidad(Combo_localidad.getValue());
-            mUsuario.setCalle(Campo_calle.getText().trim());
-            Direccion direccion = new Direccion(IdDir, mUsuario.getCalle(), Campo_codigo.getText().trim(), mUsuario.Municipio.getId(), mUsuario.Estado.getId(), mUsuario.Localidad.Id);
-            mUsuario.direccion = direccion;
-            try {
-                if (InsertarActualizarUsuario(mUsuario)) {
-                    if (OPERACION == 2) {
-                        CrearAlerta("¡Usuario actualizado!", "El usuario: " + mUsuario.getNombre() + " ha sido actualizado", Alert.AlertType.INFORMATION);
-                    } else {
-                        CrearAlerta("¡Usuario registrado!", "El usuario: " + mUsuario.getNombre() + " ha sido registrado", Alert.AlertType.INFORMATION);
-                    }
-                    OPERACION = 0;
-                    cerrarModalMenuUsuarios();
-                } else {
-                    CrearAlerta("Error", "Ha ocurrido un error al registrar al usuario", Alert.AlertType.WARNING);
+            if(camposValidos()){
+                int IdNombre = 0;
+                int IdDir = 0;
+                mUsuario = new Usuario();
+                if (editUsuario != null) {
+                    mUsuario.IdUsuario = editUsuario.IdUsuario;
+                    IdNombre = editUsuario.nombre.getIdNombre();
+                    IdDir = editUsuario.direccion.IdDireccion;
                 }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+                mUsuario.setNombre(Campo_nombre.getText().trim());
+                mUsuario.setApellidoPaterno(Campo_apellido_paterno.getText().trim());
+                mUsuario.setApellidoMaterno(Campo_apellido_materno.getText().trim());
+                mUsuario.setSexo(Combo_sexo.getValue());
+                mUsuario.setEdad(Integer.parseInt(Campo_edad.getText().trim()));
+                mUsuario.setCorreo(Campo_correo.getText().trim());
+                Nombres nombre = new Nombres(IdNombre, mUsuario.getNombre(), mUsuario.getApellidoPaterno(), mUsuario.getApellidoMaterno());
+                mUsuario.nombre = nombre;
+                try {
+                    String contraseniacifrada = ClaseCifrarContrasenia.encript(Campo_contrasenia.getText().trim());
+                    mUsuario.setContrasenia(contraseniacifrada);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                mUsuario.setCurp(Campo_curp.getText().trim());
+                mUsuario.setTelefono(new BigInteger(Campo_telefono.getText().trim()));
+                mUsuario.setOcupacion(Combo_ocupacion.getValue());
+                mUsuario.setGradoEscolar(Combo_grado.getValue());
+                mUsuario.setCodigoPostal(Integer.parseInt(Campo_codigo.getText().trim()));
+                mUsuario.setEstado(Combo_estado.getValue());
+                mUsuario.setMunicipio(Combo_municipio.getValue());
+                mUsuario.setLocalidad(Combo_localidad.getValue());
+                mUsuario.setCalle(Campo_calle.getText().trim());
+                Direccion direccion = new Direccion(IdDir, mUsuario.getCalle(), Campo_codigo.getText().trim(), mUsuario.Municipio.getId(), mUsuario.Estado.getId(), mUsuario.Localidad.Id);
+                mUsuario.direccion = direccion;
+                try {
+                    if (InsertarActualizarUsuario(mUsuario)) {
+                        if (OPERACION == 2) {
+                            CrearAlerta("¡Usuario actualizado!", "El usuario: " + mUsuario.getNombre() + " ha sido actualizado", Alert.AlertType.INFORMATION);
+                        } else {
+                            CrearAlerta("¡Usuario registrado!", "El usuario: " + mUsuario.getNombre() + " ha sido registrado", Alert.AlertType.INFORMATION);
+                        }
+                        OPERACION = 0;
+                        cerrarModalMenuUsuarios();
+                    } else {
+                        CrearAlerta("Error", "Ha ocurrido un error al registrar al usuario", Alert.AlertType.WARNING);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }else{
+                Alert alert = alertas.CrearAlertaError("Campos inválidos", "Verifica la información de los campos");
+                alert.showAndWait();
             }
         });
+
+        Campo_correo.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 30) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_contrasenia.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 12) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_curp.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 18) {
+                return null;
+            }
+            change.setText(change.getText().toUpperCase());
+            return change;
+        }));
+
+        Campo_telefono.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 10) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_nombre.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 30) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_edad.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 2) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_apellido_paterno.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 20) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_apellido_materno.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 20) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_calle.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 50) {
+                return null;
+            }
+            return change;
+        }));
+
+        Campo_codigo.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 5) {
+                return null;
+            }
+            return change;
+        }));
     }
 
     private void ConfigurarCombos() throws SQLException {
